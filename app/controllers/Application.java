0a2d9bb74045379999809f39ac0852658146daf1
unknown <Mobile@monjones-PC.(none)>
2012-11-12 17:01:14 -0500

    init

---
+++ app/controllers/Application.java
@@ -0,0 +1,54 @@
+package controllers;
+
+import java.util.Date;
+
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.node.ArrayNode;
+import org.codehaus.jackson.node.JsonNodeFactory;
+import org.codehaus.jackson.node.ObjectNode;
+
+import play.libs.Json;
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.index;
+
+public class Application extends Controller {
+  
+  public static Result index() {
+    return ok(index.render("Your new application is ready."));
+  }
+    
+  public static Result ListDocuments(String userName)
+  {      
+      return ok(populateFakeDocuments());
+  }
+  
+  private static ArrayNode populateFakeDocuments()
+  {
+      JsonNode json = request().body().asJson();
+      ObjectNode result = Json.newObject();
+      ArrayNode array = JsonNodeFactory.instance.arrayNode();
+      
+      result.put(JSONDocumentFields.PAGE_COUNT, 2);
+      result.put(JSONDocumentFields.PRINT_STATUS, "DEPRECATED");
+      result.put(JSONDocumentFields.N_UP, 1);
+      result.put(JSONDocumentFields.CONVERSION_STATUS, "CONVERSION_SUCCESS");
+      result.put(JSONDocumentFields.ID, 1);
+      result.put(JSONDocumentFields.TITLE, "test");
+      result.put(JSONDocumentFields.COPIES, 1);
+      result.put(JSONDocumentFields.SOURCE, "web");
+      result.put(JSONDocumentFields.EMAIL, "");
+      result.put(JSONDocumentFields.COLOR, true);
+      result.put(JSONDocumentFields.PRIORITY, 0);
+      result.put(JSONDocumentFields.DESCRIPTION, "");
+      result.put(JSONDocumentFields.USER_ID, "sdicken");
+      result.put(JSONDocumentFields.FILENAME, "PDF.pdf");
+      result.put(JSONDocumentFields.UPLOAD_DATE, new Date().getTime());
+      result.put(JSONDocumentFields.MIMETYPE, "application/pdf");
+      result.put(JSONDocumentFields.DUPLEX, "off");      
+      
+      array.add(result);
+      
+      return array;
+  }
+}
---
+++ app/controllers/JSONDocumentFields.java
@@ -0,0 +1,22 @@
+package controllers;
+
+public class JSONDocumentFields
+{
+    public static final String PAGE_COUNT = "pageCount";
+    public static final String PRINT_STATUS = "printStatus";
+    public static final String N_UP = "nUp";
+    public static final String CONVERSION_STATUS = "conversionStatus";
+    public static final String ID = "id";
+    public static final String TITLE = "title";
+    public static final String COPIES = "copies";
+    public static final String SOURCE = "source";
+    public static final String EMAIL = "email";
+    public static final String COLOR = "color";
+    public static final String PRIORITY = "priority";
+    public static final String DESCRIPTION = "description";
+    public static final String USER_ID = "userId";
+    public static final String FILENAME = "filename";
+    public static final String UPLOAD_DATE = "uploadDate";
+    public static final String MIMETYPE = "mimeType";
+    public static final String DUPLEX = "duplex";
+}
